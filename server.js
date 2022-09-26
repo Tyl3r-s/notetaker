d39e18b95775209296db50ece45e19552d80e94d
Tyler <Tyl3r.s@outlook.com>
2022-09-26 12:32:02 -0400

    done

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// paths to apiRoutes and htmlRoutes
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,5 +14,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
